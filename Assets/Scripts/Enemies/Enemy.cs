7ac01814265badadf49e7a3224990f5f81672101
Ed Marrs <Edward.Marrs1@marist.edu>
2019-09-24 17:56:28 -0400

    Base Mechanics
    Contains base mechanics for C&C.

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public int health=3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(health <=0) {
+		   Destroy(gameObject);
+	   }
+    }
+	public void TakeDamage(int damage){
+		health-=damage;
+		Debug.Log("Damage Taken");
+	}
+}
---
+++ Assets/Scripts/Player/swordAttackScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class swordAttackScript : MonoBehaviour
+{
+	private float timeBtwnAttack;
+	public float startTimeBtwAttack;
+	
+	public Transform attackPos;
+	public float attackRange;
+	public LayerMask whatIsEnemies;
+	public int damage=1;
+	
+    // Start is called before the first frame update
+	void Update(){
+	   if(timeBtwnAttack<=0){
+		   
+		   if(Input.GetKey(KeyCode.Z)){
+			   Debug.Log("Attacking...");
+			  Collider2D[] enemiesToDamage=Physics2D.OverlapCircleAll(attackPos.position,attackRange,whatIsEnemies);
+			  for(int i = 0; i<enemiesToDamage.Length;i++)
+			  {
+				  enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+			  }
+			  timeBtwnAttack=startTimeBtwAttack;
+		   }
+		   
+	   }
+	   else{
+		   timeBtwnAttack-=Time.deltaTime;
+		}
+	}
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color=Color.red;
+		Gizmos.DrawWireSphere(attackPos.position,attackRange);
+	}
+}
