7b3e2602239b49da6c26c1f0405b6c7e275a2afa
Ed Marrs <Edward.Marrs1@marist.edu>
2019-10-02 21:00:27 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/EdMarrs/ChaoticCatacombs""
    This reverts commit a2e6e3c1b10439d70c029d83c035db627be6e327.

---
+++ Assets/LevelMaps/newTestSet.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="newTestSet" tilewidth="16" tileheight="16" tilecount="40" columns="8">
+ <image source="simpleGraphics_tiles16x16.png" width="128" height="80"/>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="0">
+    <polygon points="0,0 15.9091,-0.0909091 16,16 0.0909091,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="0">
+    <polygon points="0,0 16,0 16.0909,16 0.181818,16.0909"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="0.0909091">
+    <polygon points="0,0 15.9091,-0.0909091 15.9091,15.8182 -0.181818,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.272727" y="0.0909091">
+    <polygon points="0,0 16.2727,-0.272727 16.2727,15.9091 0.363636,15.8182"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.0909091">
+    <polygon points="0,0 15.9091,0.181818 15.9091,16.0909 0,16.0909"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
