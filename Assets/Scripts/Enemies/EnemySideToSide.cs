7aa6b42124e59219cfd3083c955541ba7c4f9162
Ed Marrs <Edward.Marrs1@marist.edu>
2019-10-04 17:22:14 -0400

    Update 10/4
    -Added Player HP and Collision
    -Enemies can now move
    -Enemies can now move through player
    -Player can take damage and die
    -Death cam follows main cam, and is used when player dies

---
+++ Assets/Scripts/Enemies/EnemySideToSide.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySideToSide : MonoBehaviour{
+    public float speed;
+	public float distanceToMove;
+    private float locationCounter = 0;
+    private bool isGoingRight=true;
+	private Rigidbody2D rb2d;
+	void Awake()
+    {
+		
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+	
+    void FixedUpdate()
+    {
+       
+        CheckFlip();
+        
+    }
+	 void CheckFlip()
+    {
+		if(isGoingRight==true){
+          Vector2 movement = new Vector2 (1, 0);
+          rb2d.velocity = (movement * speed);
+        }
+        else{
+          Vector2 movement = new Vector2 (-1, 0);
+          rb2d.velocity = (movement * speed);
+        }
+        
+        locationCounter++;
+        
+        if(locationCounter >=distanceToMove){
+          if(isGoingRight==true){
+            isGoingRight=false;
+          }
+          else{
+            isGoingRight=true;
+          }
+          locationCounter=0;
+        }
+	}
+}
---
+++ Assets/Scripts/Player/PlayerIsHit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIsHit : MonoBehaviour
+{
+	public int health=3;
+	public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	  private void OnTriggerEnter2D(Collider2D c)
+    {
+		if(c.GetComponent<Collider2D>().tag == "enemy")
+         {
+        
+           health-=1;
+           Debug.Log("the player has been hit");
+        
+		 }
+    }
+	
+    void Update()
+    {
+       if(health <=0) {
+		   Destroy(player);
+		   Debug.Log("Player Killed");
+	   }
+    }
+	
+   }
+	
+
+
