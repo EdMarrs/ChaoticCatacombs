7d45578556254c4e17eff771101c1809c932d58e
Ed Marrs <Edward.Marrs1@marist.edu>
2019-10-08 13:43:09 -0400

    10/8-Spawner
    -Implementing Enemy Spawner

---
+++ Assets/Scripts/spawnerCollide.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnerCollide : MonoBehaviour
+{
+	public int numberOfEnemyTypes;
+
+    public GameObject[] enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		for(int i = 0; i < enemies.Length; i++)
+       {
+
+            if (i < numberOfEnemyTypes)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+
+       }
+        if(c.GetComponent<Collider2D>().tag == "player")
+         {
+			 
+		 }
+    }
+}
