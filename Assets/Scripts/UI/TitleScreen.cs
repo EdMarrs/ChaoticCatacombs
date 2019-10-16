fe99f2fb11dca58c7eed4f9c98219bfd41fee4dc
M Tinnirello <tmarinel7777@gmail.com>
2019-10-16 16:07:24 -0400

    Menu UI changes
    Fixed up Main Menu UI, Game Over UI

---
+++ Assets/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**** Takes care of Scene Management in the Main Menu ****/
+public class TitleScreen : MonoBehaviour
+{
+    /*** Plays game upon hitting "Start" button ***/
+    public void play()
+    {
+        // finds next scene's index by the build
+        // File -> Build Settings -> "Scenes in Build"
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /*** Quits game upon hitting "Quit" button ***/
+    public void quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
