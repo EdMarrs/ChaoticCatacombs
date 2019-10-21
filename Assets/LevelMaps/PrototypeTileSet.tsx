ce5b3a081d9744c954aed3a3fb39134eb8996c8c
LiamMarist <31492063+LiamMarist@users.noreply.github.com>
2019-10-21 15:15:03 -0400

    Level 1 Updates
    -Added the larger level quadrants
    -Added platforms that player can jump and fall through
    -Readjusted enemy spawner locations
    -Commented out code for player heats, was causing errors
    -Platforms still need to be adjusted and tweaked

---
+++ Assets/LevelMaps/PrototypeTileSet.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="PrototypeTileSet" tilewidth="16" tileheight="16" tilecount="40" columns="8">
+ <image source="simpleGraphics_tiles16x16.png" width="128" height="80"/>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="0.03125" y="-0.03125">
+    <polygon points="0,0 15.9688,0 15.9688,16.0313 -0.0625,16.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="-0.09375">
+    <polygon points="0,0 16.1563,0.0625 16.0938,16.1563 0.09375,16.1875"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
