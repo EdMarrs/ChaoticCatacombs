389bc8e9b987999c287daa056a2416fee8a893d0
LiamMarist <31492063+LiamMarist@users.noreply.github.com>
2019-10-23 02:56:35 -0400

    Boss Movement
    -added Temporary Boss movement, it is a bit rough and has some issues
    - added a dusk background to hide skull in background

--- Assets/Scripts/Boss1Phase3.cs
+++ Assets/Scripts/Boss1Phase3.cs
@@ -7,22 +7,195 @@ public class Boss1Phase3 : MonoBehaviour
     /*
      * This is the script handling the 3rd phase of the Skeleton Boss
      * The hands of the Boss will follow the pattern of moving between 2 points being the platforms
-     * After that movement the hands will move in a clapping motion
+     * After that movement the hands will move in a third motion
      */
 
     public Vector3 pos1 = new Vector3(0, 0, 0);
     public Vector3 pos2 = new Vector3(0, 0, 0);
+    public Vector3 newPos1 = new Vector3(0, 0, 0);
+    public Vector3 newPos2 = new Vector3(0, 0, 0);
+    public Vector3 clapPos1 = new Vector3(0, 0, 0);
+    public Vector3 clapPos2 = new Vector3(0, 0, 0);
+    public Vector3 rotPos = new Vector3(0, 0, 0);
+    public Vector3 origRotPos = new Vector3(0, 0, 0);
+    public Transform currentPos;
+    
+
     public float speed;
+    
+    private bool first = false;
+    private bool second = false;
+    private bool third = false;
+    private bool rotationTest = false;
+    private bool fourth = false;
+    private bool fifth = false;
 
 
 
     void Start()
     {
+        /* Once player completes phase 2
+         * hand health reference will be reset in the Hand Manager
+         * 
+         * 
+         */
+
+
+        
         
     }
 
     void Update()
     {
-        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
+       // currentPos = transform.position;
+
+        if (first == false)
+        {
+            StartCoroutine(A());
+            BackNForth();
+        }
+
+        if (second == false && first == true)
+        {
+
+            if (rotationTest == false)
+            {
+                RotateHand();
+            }
+
+            StartCoroutine(B());
+     
+        }
+
+        if ( third == false && first == true)
+        {
+            
+            StartCoroutine(C());
+            MoveToClap();
+        }
+
+        if (fourth == false && third == true)
+        {
+            StartCoroutine(D());
+            ClapHands();
+            
+        }
+
+        if (fifth == false && fourth == true)
+        {
+            StartCoroutine(E());
+            FinalMovement();
+        }
+
+        if(first && second && third && fourth && fifth == true)
+        {
+            ResetRotateHand();
+           
+           
+        }
+        /*
+        print("test: " + test);
+        print("first: " + first);
+        print("third: " + third);
+        print("fourth: " + fourth);
+        print("rotationTest: " + rotationTest);
+
+        */
     }
+
+
+    // These are a bit Unreliable, they don't always start at the same time, causing issues... 
+
+    IEnumerator A()
+    {
+      
+        yield return new WaitForSeconds(5f);
+        first = true;
+
+    }
+
+    IEnumerator B()
+    {
+
+        yield return new WaitForSeconds(1f);
+        second = true;
+
+    }
+
+    IEnumerator C()
+    {
+
+        yield return new WaitForSeconds(1f);
+        third = true;
+
+    }
+
+    IEnumerator D()
+    {
+        // keep around 10f
+        yield return new WaitForSeconds(5f);
+        fourth = true;
+ 
+    }
+
+    IEnumerator E()
+    {
+        
+        yield return new WaitForSeconds(1f);
+        fifth = true;
+
+    }
+
+    void BackNForth()
+    {
+        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
+        fourth = false;
+        fifth = false;
+    }
+
+    void RotateHand()
+    {
+        transform.Rotate(rotPos);
+        rotationTest = true;
+    }
+
+    void ResetRotateHand()
+    {
+        transform.Rotate(origRotPos);
+        ResetBools();
+
+    }
+
+    void MoveToClap()
+    {
+        transform.position = Vector3.Lerp(newPos1, newPos2, Mathf.PingPong(Time.time * speed, 1.0f));
+    }
+
+    void ClapHands()
+    {
+        transform.position = Vector3.Lerp(clapPos1, clapPos2, Mathf.PingPong(Time.time * speed, 1.0f));
+    }
+
+    void FinalMovement()
+    {
+        transform.position = Vector3.Lerp(pos2, clapPos2, Mathf.PingPong(Time.time * speed, 1.0f));
+    }
+
+    void ResetBools()
+    {
+        first = false;
+        second = false;
+        third = false;
+        rotationTest = false;
+        fourth = false;
+        fifth = false;
+    }
+
+    /*
+    void StopDoingThings()
+    {
+
+        return;
+    }
+    */
 }
