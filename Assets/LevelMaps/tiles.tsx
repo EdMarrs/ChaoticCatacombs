62b76f1b08f232659168962ef57d875c9a25db06
Matt.Z <matthew.zaccaro1@marist.edu>
2019-10-27 14:04:18 -0400

    Level1 V2 Retexture
    Moved spawners, recreated platform's using Liam's script, fully retextured level
    
    TODO: Debug layering issue with player moving behind pillars and decorations

---
+++ Assets/LevelMaps/tiles.tsx
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tiles" tilewidth="16" tileheight="16" tilecount="884" columns="17">
+ <image source="Tilesetv3.png" width="272" height="832"/>
+ <tile id="19">
+  <objectgroup draworder="index">
+   <object id="1" x="8.125" y="0.272727">
+    <polygon points="0,-0.0909091 0,7.72727 -7.90909,7.81818 -7.90909,15.7273 8,15.7273 8,-0.181818"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="0">
+    <polygon points="0,0 7.90909,0.0909091 7.81818,8.09091 15.7273,8 15.8182,15.9091 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="0" width="15.8182" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="0" width="16.1818" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16.0909" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="51">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16.0909" height="16.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16.0909" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.0909091" width="16" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="72">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="0" width="16.0909" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16.0909" height="15.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0625" y="0.0625" width="15.875" height="15.8125"/>
+  </objectgroup>
+ </tile>
+ <tile id="87">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0625" y="0.125" width="16" height="15.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="88">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="0.125" width="15.875" height="15.8125"/>
+  </objectgroup>
+ </tile>
+ <tile id="89">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="0" width="16.1136" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="162">
+  <objectgroup draworder="index">
+   <object id="1" x="1.0625" y="0.125" width="14.9375" height="10.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="163">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="0.125" width="15.8125" height="10.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="164">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="0.0625" width="14.875" height="10.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="323">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.272727" y="-0.0909091" width="16.3636" height="16.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="324">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="0.181818" width="15.8182" height="15.7273"/>
+  </objectgroup>
+ </tile>
+ <tile id="340">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.0909091" width="15.9091" height="15.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="341">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="1.09091" width="15.9091" height="14.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="342">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="343">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="-5.55112e-17" width="16" height="15.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="344">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="15.9091" height="15.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="357">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.0909091" width="15.9091" height="15.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="361">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="0" width="16" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="374">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="0.181818" width="15.8182" height="15.7273"/>
+  </objectgroup>
+ </tile>
+ <tile id="378">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="0" width="16.0909" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="391">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="0.181818" width="15.6364" height="15.7273"/>
+  </objectgroup>
+ </tile>
+ <tile id="395">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="15.9091" height="15.9091"/>
+  </objectgroup>
+ </tile>
+</tileset>
