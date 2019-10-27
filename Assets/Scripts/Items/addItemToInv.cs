582470565a87831878ccba74445186995672b3c0
Ed Marrs <Edward.Marrs1@marist.edu>
2019-10-27 18:35:39 -0400

    Update 10/27-Items!
    -Added Peripat Of HP: Adds +1 Max HP
    -Added Amulet of Strength: +1 Damage

---
+++ Assets/Scripts/Items/addItemToInv.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addItemToInv : MonoBehaviour
+{
+    public GameObject itemToAddToInv;
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.GetComponent<Collider2D>().tag == "Player")
+        {
+                
+        }
+    }
+}
