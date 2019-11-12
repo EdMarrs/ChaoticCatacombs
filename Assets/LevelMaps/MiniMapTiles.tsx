889ab6252ae3583e5a4beb233cce0d71865f23de
LiamMarist <31492063+LiamMarist@users.noreply.github.com>
2019-11-12 17:29:12 -0500

    Mini Map
    added new mini map & camera
    player, enemies and items are displayed on map

---
+++ Assets/LevelMaps/MiniMapTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="MiniMapTiles" tilewidth="16" tileheight="16" tilecount="18" columns="6">
+ <image source="MiniMapTileSet.png" width="96" height="48"/>
+</tileset>
