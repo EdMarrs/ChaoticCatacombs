862241a68108495f9da6bbf83bc54bdcfcd76e66
Joe C <joecannarozzo@yahoo.com>
2019-11-16 19:43:32 -0500

    Level3 in Unity.

---
+++ Assets/LevelMaps/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="tileset" tilewidth="16" tileheight="16" tilecount="884" columns="17">
+ <image source="Tilesetv3.png" width="272" height="832"/>
+</tileset>
