9ca4b7bf3878d732715593532d73050fb6acdc8b
Ed Marrs <Edward.Marrs1@marist.edu>
2019-11-20 17:00:56 -0500

    Revert "Merge remote-tracking branch 'origin/MapAddition'"
    This reverts commit 106093caf2b82dec55ec6922ddfb446272df50eb, reversing
    changes made to 42a51db0fc417536d4a5568eb1ff77177744ff1e.

--- Assets/Scripts/Enemies/onSlimeDeath.cs
+++ Assets/Scripts/Enemies/onSlimeDeath.cs
@@ -7,31 +7,17 @@ public class onSlimeDeath : MonoBehaviour
 	private int hp;
 	public GameObject miniSlime;
     private Animator anim;
-
-    public int chanceToSpawnHealth = 50;
-    public GameObject healthToSpawn;
-    private int itemChanceRoll;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        itemChanceRoll = Random.Range(0, 100);
         anim = GetComponent<Animator>();
        
     }
 	void Update(){
 		hp = gameObject.GetComponent<Enemy>().health;
 		if(hp <= 0){
-            
-
-                // spawns health based on random chance
-                if (itemChanceRoll >= 100 - chanceToSpawnHealth)
-                {
-
-                    Instantiate(healthToSpawn, new Vector3(transform.position.x, transform.position.y + 1.1f), Quaternion.identity);
-                }
-
-                Debug.Log("Hit");
+			Debug.Log("Hit");
        
             anim.SetBool("isDead", true);
             //Instantiate(miniSlime, transform.position + new Vector3(0, 0, 0), Quaternion.identity);
