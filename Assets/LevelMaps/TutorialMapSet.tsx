2b8eb5523ad4b46cba3b65ef7e8339214c934df1
BaileyVillipart1 <Bailey.Villipart1@marist.edu>
2019-11-19 21:04:21 -0500

    Recommit

--- Assets/LevelMaps/TutorialMapSet.tsx
+++ Assets/LevelMaps/TutorialMapSet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.5" name="TutorialMapSet" tilewidth="16" tileheight="16" tilecount="40" columns="8">
- <image source="../Documents/GitHub/ChaoticCatacombs/Assets/LevelMaps/simpleGraphics_tiles16x16.png" width="128" height="80"/>
+ <image source="simpleGraphics_tiles16x16.png" width="128" height="80"/>
 </tileset>
