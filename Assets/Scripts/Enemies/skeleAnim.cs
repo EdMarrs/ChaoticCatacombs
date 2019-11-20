036c07cf873abee583d87718a2cd19080b209ee2
Ed Marrs <Edward.Marrs1@marist.edu>
2019-11-20 17:29:15 -0500

    Revert "Revert "Merge branch 'Outro'""
    This reverts commit 789aab64eee9e013fc1f49a4458173a95235e582.

---
+++ Assets/Scripts/Enemies/skeleAnim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class skeleAnim : MonoBehaviour
+{
+    public Sprite[] images;
+    public BoxCollider2D line;
+    private BoxCollider2D playerRef;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRef = GameObject.Find("Player").GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (line != null)
+        {
+        	if (line.IsTouching(playerRef))
+        	{
+        		Destroy(line);
+        		StartCoroutine(ripSkele());
+        	}
+        }
+    }
+
+    public IEnumerator ripSkele()
+    {
+    	for (int i = 0; i < 5; i++)
+    	{
+    		for (int j = 0; j < 250 + (25 * i); j++)
+    			yield return null;
+    		GetComponent<SpriteRenderer>().sprite = images[i];
+    	}
+    }
+}
---
+++ Assets/Scripts/UI/dialogScripts/dialogTrigger2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogTrigger2 : MonoBehaviour
+{
+   public Dialog dialog;
+
+    public void triggerDialog()
+    {
+    	FindObjectOfType<printDialogEnd>().StartDialog(dialog);
+    }
+}
