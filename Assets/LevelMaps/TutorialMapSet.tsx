7f48a362befe5104bbb0b3570784ba4b67f219b2
Joe C <joecannarozzo@yahoo.com>
2019-11-20 01:17:04 -0500

    Tutorial Level Progress.

--- Assets/LevelMaps/TutorialMapSet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="TutorialMapSet" tilewidth="16" tileheight="16" tilecount="40" columns="8">
- <image source="simpleGraphics_tiles16x16.png" width="128" height="80"/>
-</tileset>
