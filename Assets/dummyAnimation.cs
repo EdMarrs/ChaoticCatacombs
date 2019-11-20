a6eaf0e9865214a4a5712adaf3e05b4197ce6690
Ed Marrs <Edward.Marrs1@marist.edu>
2019-11-20 10:35:08 -0500

    Dummy Animation Stuff

---
+++ Assets/dummyAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dummyAnimation : MonoBehaviour
+{
+    public int health = 5;
+    public GameObject hay;
+    private Rigidbody2D rb;
+    public float thrust;
+    private Animator anim;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        anim.SetBool("isHit",true);
+        Instantiate(hay, transform.position, Quaternion.identity);
+
+        health -= damage;
+
+        Debug.Log("Damage Taken");
+        
+    }
+
+
+}
