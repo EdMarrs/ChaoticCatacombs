243a22e67ed729c5c65220330c5d05d0d399f5e7
Ed Marrs <Edward.Marrs1@marist.edu>
2019-11-20 05:22:25 -0500

    Update 11/20-fixed Bat anim
    -Gave bat ability to flip

---
+++ Assets/AIFlipping.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFlipping : MonoBehaviour
+{
+   
+    void Start()
+    {
+     
+    }
+    void Update()
+    {
+        float playerPos = GameObject.Find("Player").GetComponent<Transform>().position.x;
+        float batPos = GetComponent<Transform>().position.x;
+        float side = playerPos - batPos;
+        if (side >= 0)
+        {
+            GetComponent<SpriteRenderer>().flipX =true;
+        }
+        else if (side< 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
