78f8b129e882555d0d72d4e4dfb951eb63211beb
LiamMarist <31492063+LiamMarist@users.noreply.github.com>
2019-11-20 04:24:10 -0500

    Health pickup
    Enemies Drop health now, it's just a thing they do.

---
+++ Assets/healthPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthPickup : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.GetComponent<Collider2D>().tag == "Player")
+        {
+           
+            if (GameObject.Find("playerCollider").GetComponent<PlayerIsHit>().health < 5)
+            {
+                GameObject.Find("playerCollider").GetComponent<PlayerIsHit>().health = GameObject.Find("playerCollider").GetComponent<PlayerIsHit>().health + 1;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
