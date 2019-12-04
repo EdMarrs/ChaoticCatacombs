8da44f1e3dde8e5a52835d75302bea13c46a37a3
Ed Marrs <Edward.Marrs1@marist.edu>
2019-12-03 20:42:58 -0500

    Update 12/3-FINAL PUSH
    -Added Credits
    -Added Intro/Outro Skip
    -Raised Hitbox of Roof in tutorial
    -Fixed Escape Key
    -Removed Extra Platform
    -Changed Jump boots to peripat of HP
    
    WE DONE FOLKS

--- Assets/SkipToMenu.cs
+++ Assets/SkipToMenu.cs
@@ -1,20 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class QuitGame : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class SkipToMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
-    public void Update()
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown("s"))
         {
+            
+
             UnityEngine.SceneManagement.SceneManager.LoadScene(1);
         }
     }
